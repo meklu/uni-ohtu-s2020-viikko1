533bda604555c7149d7d7e1006b2193a4821fe8d
Melker Narikka <melker.narikka@helsinki.fi>
2020-11-02 18:46:49 +0200

    korjaa testi

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -75,6 +75,6 @@ public class Varasto {
 
     // --- Merkkijonoesitys Varasto-oliolle: ----
     public String toString() {
-        return ("asdf = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
+        return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
     }
 }
